0c9f1ad26a3b3bb3afe0679fa89e2638d6621856
igor k <igor.kurilin@gmail.com>
2017-08-23 13:19:47 +0800

    full server implemented, started exchange design

--- server.py
+++ server.py
@@ -1,100 +1,50 @@
 # Coinigy server module bn
-from socketclusterclient import Socketcluster
 import logging
-from time import sleep
-import sys
+from coinigy_api_rest import CoinigyREST
+from coinigy_api_websocket import CoinigyWebSocket
+from collections import namedtuple
 
-class CoinigySoocket():
-    def __init__(self):
-        self.logger=logging.getLogger('root')
-        self.__reset()
+# Coinigy full API (REST + WebSocket)
+class CoinigyAPI :
+    def __init__(self, apiKey, apiSecret, reconnect=True, wsURL='wss://sc-02.coinigy.com/socketcluster/', restURL='https://api.coinigy.com/api/v1'):
+        self.logger = logging.getLogger('root')
+        self.logger.debug("Initialize Coinigy API")
+        # WEB SOCKET
+        self.ws = CoinigyWebSocket()
+        self.ws.connect(apiKey, apiSecret, wsURL) # Connect to websocket
+        # REST
+        credentials = namedtuple('credentials', ('api','secret','endpoint'))
+        credentials.api = apiKey
+        credentials.secret = apiSecret
+        credentials.endpoint =  restURL
 
-    def connect(self, apiKey, apiSecret, wsURL='"wss://sc-02.coinigy.com/socketcluster/"', channelcode = "TRADE-OK--BTC--CNY", channelHandler = None ):
-        # Connects to the Coinigy API websocket and initializes data stores
-        self.logger.debug("Connecting Websocket")
-        self.logger.info("Conecting to %s" % wsURL)
-        self.apiKey = apiKey
-        self.apiSecret = apiSecret
-        self.bAuthenticated = False # not yet authenticated
-        self.bError = False # no errors yet
-        self.socket = Socketcluster.socket(wsURL) # create socket connection
-
-        # set basic event listeners
-        self.socket.setBasicListener(self.__onconnect, self.__ondisconnect, self.__onconnecterror)
-        # set authentication listeners
-        self.socket.setAuthenticationListener(self.__onsetauthentication, self.__onauthentication)
-        # initiate connecting sequence
-        self.socket.connect()
-
-        # wait for authentication
-        conn_timeout = 5
-        while(not self.bAuthenticated and conn_timeout and not self.bError) :
-            sleep(1)
-            conn_timeout -= 1
-
-        if not conn_timeout or self.bError:
-            self.logger.error("Couldn't authenticate the connection. Exiting")
-            self.exit()
-            sys.exit(1)
-
-        #subscribe to channels
-        if channelHandler :   self.__subscribe(channelcode, channelHandler )
-        else: self.__subscribe(channelcode,self.__defaultchannelmessagehandler)
-
-    def __defaultchannelmessagehandler(self, key, object):
-        self.logger.info("Got data for key %s" % key)
-        self.logger.info(object)
+        self.rest = CoinigyREST(credentials)
 
 
-    # SetAuthentication event listener
-    def __onsetauthentication(self, socket, token):
-        self.logger.info("Authentication token received %s" % token)
-        self.socket.setAuthtoken(token)
+    #get all exchange pairs (asynchronus)
+    def getFxPairs(self, exchangeCode):
+        return self.rest.markets(exchangeCode)
 
-    def __onsubscribtionerrorhandler(self, channel, error, object):
-        if error is '':
-            self.logger.info("Subscribed successfully to channel %" % channel)
-        else:
-            self.logger.error("Cannot subscribe for channel %s" & channel)
-            self.logger.info(error)
-            self.bError = True
+    # get all available exchanges (synchronus)
+    def getAllExchanges(self):
+        return self.rest.exchanges()
 
-    # subscribe for a channel
-    def __subscribe(self, channelCode, messageHandler):
-        self.socket.subscribeack(channelCode, self.__onsubscribtionerrorhandler)
-        self.socket.onchannel(channelCode, messageHandler)
+    def getAsks(self, exchangeCode, pairCode):
+        return self.rest.asks(exchangeCode, pairCode)
 
-    # authentication acknowledgement handler
-    def __authenticationacknoledgmenthandler(self,eventname, error, object ):
-        self.logger.info("authentication ack got called %s" % str(error))
-        if error is '':
-            # set authentication flag
-            self.authenticated = True
-        else:
-            self.bError = True
-            self.logger.error("Cannot authenticate connection %s" % str(error))
-            self.logger.info(error)
+    def getBids(self, exchangeCode, pairCode):
+        return self.rest.bids(exchangeCode, pairCode)
+
+    def getOrders(self, exchangeCode, pairCode):
+        return self.rest.orders(exchangeCode, pairCode)
+
+    # susbcribe for the channel
+    # channelcode: "METHOD-EXCHANGECODE--PRIMARYCURRENCY--SECONDARYCURRENCY"
+    # supported methods: TRADE, ORDER, NEWS, BLOCK, TICKER
+    def subscribe(self, channelcode, channelhandler):
+        return self.ws.subscribe(channelcode, channelhandler)
 
 
-    # Authentication event listener
-    def __onauthentication(self, socket, isauthenticated):
-        self.logger.info("Authenticated is %s" % str(isauthenticated))
-        # send cridentials to the server
-        api_credentials = {
-            "apiKey": self.apiKey,
-            "apiSecret": self.apiSecret
-        }
-        socket.emitack("auth", api_credentials, self.__authenticationacknoledgmenthandler)
-
-    # onConnect event handler
-    def __onconnect(self, socket):
-        self.logger.info("on connect got called")
-
-    # onDisconnect event handler
-    def __ondisconnect(self, socket):
-        self.logger.info("on disconnect got called")
-
-    # onConnectError event handler
-    def __onconnecterror(self, socket, error):
-        self.logger.info("On connect error got called %s" % str(error))
-
+# executing code
+if __name__ == '__main__':
+    pass
